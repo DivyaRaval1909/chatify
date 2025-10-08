cd0828fb277df31503771ea46435e7707ac83a44
Divya Raval <69576345+DivyaRaval1909@users.noreply.github.com>
2025-10-09 02:01:02 +0530

    Update message.controller.js

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -6,6 +6,7 @@ import User from "../models/User.js";
 export const getAllContacts = async (req, res) => {
   try {
     const loggedInUserId = req.user._id;
+    
     const filteredUsers = await User.find({ _id: { $ne: loggedInUserId } }).select("-password");
 
     res.status(200).json(filteredUsers);
